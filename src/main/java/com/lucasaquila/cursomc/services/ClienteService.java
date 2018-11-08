e36946c9a1e6aaeaab50426cd7074f23bc061a0f
Lucas <lucas.moura@pti.org.br>
2018-11-08 10:53:15 -0200

    Inclusão dos documentos do projeto

--- src/main/java/com/lucasaquila/cursomc/services/ClienteService.java
+++ src/main/java/com/lucasaquila/cursomc/services/ClienteService.java
@@ -13,11 +13,11 @@ import com.lucasaquila.cursomc.repositories.ClienteRepository;
 public class ClienteService {
 	
 	@Autowired
-	private ClienteRepository categoriaRepository;
+	private ClienteRepository clienteRepository;
 
 	public Cliente buscar(Integer id) {
-		Optional<Cliente> obj = categoriaRepository.findById(id);
+		Optional<Cliente> obj = clienteRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-		}
+	}
 }
