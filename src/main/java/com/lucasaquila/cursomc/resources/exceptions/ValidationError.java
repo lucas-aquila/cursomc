9a32fda9b4a810ff58bfee3b02e8d82ee0822e2a
Lucas <lucas.moura@pti.org.br>
2018-11-09 12:52:17 -0200

    Personalizando o retorno da validação

---
+++ src/main/java/com/lucasaquila/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,28 @@
+package com.lucasaquila.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String mensagem, Long timeStamp) {
+		super(status, mensagem, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+}
