5c27ae4375e58c8b5dc97141ecc54e4382424719
Lucas <lucas-aquila@outlook.com>
2018-11-13 15:20:33 -0200

    Implementando autenticação e geração do Token JWT

--- src/main/java/com/lucasaquila/cursomc/config/SecurityConfig.java
+++ src/main/java/com/lucasaquila/cursomc/config/SecurityConfig.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.lucasaquila.cursomc.security.JWTAuthenticationFilter;
+import com.lucasaquila.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +27,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	//Instanciamos a interface UserDetailsService, mas o Spring busca por uma implementação dela. 
+	//No caso implementamos a UserDetailsServiceImpl
+	@Autowired
+	private UserDetailsService userDetailsService;
 	
 	//Define quais caminhos estarão liberados para acesso por padrão.
 	//O ** define que a partir daquele caminho em diante estão liberados
@@ -55,9 +68,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		
 		//Irá garantir que nosso backend não vai criar seção de usuário
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+	
 	
 	//Definindo um bean com acesso básico de multiplas fontes( applyPermitDefaultValues ) para todos os caminhos
 	//Está permitindo acesso aos endpoints (controllers) com as configurações básicas
