3af6a1e66088d98d9e893ba8f2c7bc54c1cb23c2
Lucas <lucas-aquila@outlook.com>
2018-11-13 10:08:41 -0200

    Implementando SmtpMailService com servido do Google

--- src/main/java/com/lucasaquila/cursomc/config/DevConfig.java
+++ src/main/java/com/lucasaquila/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.lucasaquila.cursomc.services.DBService;
+import com.lucasaquila.cursomc.services.EmailService;
+import com.lucasaquila.cursomc.services.SmtpEmailService;
 
 //Configurações específicas do Profile de Test
 @Configuration
@@ -29,6 +31,11 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
---
+++ src/main/java/com/lucasaquila/cursomc/services/SmtpEmailService.java
@@ -0,0 +1,25 @@
+package com.lucasaquila.cursomc.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class SmtpEmailService extends AbstractEmailService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
+	
+	//O framework instancia automaticamente com todos os dados de email do application-xxx.properties
+	@Autowired
+	private MailSender mailSender;
+
+	@Override
+	public void sendEmail(SimpleMailMessage msg) {
+		LOG.info("Enviando email...");
+		mailSender.send(msg);
+		LOG.info("Email enviado");
+		
+	}
+
+}
