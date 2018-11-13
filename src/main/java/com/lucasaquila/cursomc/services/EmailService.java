dea6b21d870fc6eaddaba152666751301e87cf9e
Lucas <lucas-aquila@outlook.com>
2018-11-13 10:44:45 -0200

    Envio de Email HTML

--- src/main/java/com/lucasaquila/cursomc/services/EmailService.java
+++ src/main/java/com/lucasaquila/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.lucasaquila.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.lucasaquila.cursomc.domain.Pedido;
@@ -10,4 +12,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
