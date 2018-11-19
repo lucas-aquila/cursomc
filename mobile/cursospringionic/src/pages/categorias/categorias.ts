68e790f7e823723fbf51bac09ce1f82a7d361a97
Lucas <lucas-aquila@outlook.com>
2018-11-19 09:31:12 -0200

    Listando categorias

--- mobile/cursospringionic/src/pages/categorias/categorias.ts
+++ mobile/cursospringionic/src/pages/categorias/categorias.ts
@@ -1,3 +1,4 @@
+import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -15,11 +16,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    //É uma chamada assincrona, você precisa se inscrever(subscribe) para fazer algo quando a resposta chegar
+    this.categoriaService.findAll().subscribe(result => {
+      console.log(result);
+    },
+    error => {
+      console.log(error);
+    });
   }
 
 }
