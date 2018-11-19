002e44f40ab4c0e517e59fada7b14449287e42ab
Lucas <lucas-aquila@outlook.com>
2018-11-19 11:48:32 -0200

    Criando uma página de profile

--- mobile/cursospringionic/src/pages/categorias/categorias.ts
+++ mobile/cursospringionic/src/pages/categorias/categorias.ts
@@ -4,13 +4,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaDTO } from '../../models/categoria.dto';
 
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-categorias',
