3b69a4dac1ab7a61e16e404df440cefce8615113
Lucas <lucas-aquila@outlook.com>
2018-11-19 10:50:17 -0200

    Obtendo os dados do formulário de login

--- mobile/cursospringionic/src/pages/home/home.ts
+++ mobile/cursospringionic/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @IonicPage()
 @Component({
@@ -8,17 +9,17 @@ import { NavController, IonicPage, MenuController } from 'ionic-angular';
 })
 export class HomePage {
 
+  credenciais : CredenciaisDTO = {
+    email: "",
+    senha: ""
+  }
+
   constructor(
     public navCtrl: NavController,
     public menu: MenuController) {
   }
 
-  login() {
-    //setRoot navega para página sem a possibilidade de retornar para tela que chamou, 
-    //ao contrário do push que sobrepõe uma tela em cima da outra
-    this.navCtrl.setRoot('CategoriasPage');
-  }
-
+  
   //Quando entrar na página ele desabilita o menu lateral
   ionViewWillEnter(){
     this.menu.swipeEnable(false);
@@ -27,5 +28,13 @@ export class HomePage {
   //Quando sair da página ele habilita o menu novamente
   ionViewDidLeave(){
     this.menu.swipeEnable(true);
+  }
+  
+  login() {
+
+    console.log(this.credenciais);
+    //setRoot navega para página sem a possibilidade de retornar para tela que chamou, 
+    //ao contrário do push que sobrepõe uma tela em cima da outra
+    this.navCtrl.setRoot('CategoriasPage');
   }
 }
