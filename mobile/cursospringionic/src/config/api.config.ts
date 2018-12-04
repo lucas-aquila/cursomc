697e64cebedb19807864db985f199a52b9745ef3
Lucas <lucas.moura@pti.org.br>
2018-12-04 14:28:19 -0200

    Obtendo imagem da galeria

--- mobile/cursospringionic/src/config/api.config.ts
+++ mobile/cursospringionic/src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-    // baseUrl: "https://la-spring-ionic.herokuapp.com",
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://la-spring-ionic.herokuapp.com",
+    // baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/la-curso-spring-ionic",
     imageFolder: "assets/imgs"
 }
