a2a44c4dba6da26ba43410ff88a0dd5b9f938612
Lucas <lucas.moura@pti.org.br>
2018-12-04 12:08:40 -0200

    Ajuste no api.config

--- mobile/cursospringionic/src/config/api.config.ts
+++ mobile/cursospringionic/src/config/api.config.ts
@@ -1,12 +1,5 @@
 export const API_CONFIG = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     // baseUrl: "https://la-spring-ionic.herokuapp.com",
->>>>>>> 900c79e... Commit para subir no Heroku
-=======
-    // baseUrl: "https://la-spring-ionic.herokuapp.com",
->>>>>>> 900c79e... Commit para subir no Heroku
     baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/la-curso-spring-ionic",
     imageFolder: "assets/imgs"
