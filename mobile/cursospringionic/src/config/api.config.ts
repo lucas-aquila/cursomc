033fea18205c05cee980d76ef65e0c1d65a89e1c
Lucas <lucas.moura@pti.org.br>
2018-12-04 11:45:13 -0200

    Ajustes para produção

--- mobile/cursospringionic/src/config/api.config.ts
+++ mobile/cursospringionic/src/config/api.config.ts
@@ -1,4 +1,8 @@
 export const API_CONFIG = {
+<<<<<<< HEAD
+=======
+    // baseUrl: "https://la-spring-ionic.herokuapp.com",
+>>>>>>> 900c79e... Commit para subir no Heroku
     baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/la-curso-spring-ionic",
     imageFolder: "assets/imgs"
